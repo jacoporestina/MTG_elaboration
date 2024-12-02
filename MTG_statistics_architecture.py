f6cf6d8c10ee64709f954ed10ba9785d3229b61e
JacopoRestina <jacopo.restina@wur.nl>
2024-12-02 15:45:15 +0100

    Updated code statistic and plots architectural traits

--- MTG_statistics_architecture.py
+++ MTG_statistics_architecture.py
@@ -13,8 +13,8 @@ def analyze_mtg_data(file_path, output_folder, excluded_columns):
     # Print all column names in the dataset
     print("All columns in the dataset:", data.columns.tolist())
 
-    for date in data['Date'].unique():
-        date_data = data[data['Date'] == date]
+    for date in data['date'].unique():
+        date_data = data[data['date'] == date]
 
         for order_type in ['order1', 'order2']:
             order_data = date_data[~date_data[order_type].isna()]
@@ -22,7 +22,7 @@ def analyze_mtg_data(file_path, output_folder, excluded_columns):
             if not os.path.exists(date_order_folder):
                 os.makedirs(date_order_folder)
 
-            columns_to_analyze = [col for col in order_data.columns[6:] if col not in excluded_columns]
+            columns_to_analyze = [col for col in order_data.columns[8:] if col not in excluded_columns]
 
             # Print columns that are being analyzed
             print(f"Analyzing the following columns for {order_type} on {date}: {columns_to_analyze}")
@@ -35,8 +35,8 @@ def analyze_mtg_data(file_path, output_folder, excluded_columns):
                 with open(results_file_path, 'w') as results_file:
                     for element in order_data[order_type].unique():
                         element_data = order_data[order_data[order_type] == element]
-                        unique_densities = element_data['Density'].unique()
-                        groups = [element_data[element_data['Density'] == density][column].dropna() for density in unique_densities]
+                        unique_densities = element_data['density'].unique()
+                        groups = [element_data[element_data['density'] == density][column].dropna() for density in unique_densities]
                         valid_groups = [group for group in groups if len(group) >= 3]
 
                         if len(valid_groups) >= 2:
@@ -60,14 +60,14 @@ def analyze_mtg_data(file_path, output_folder, excluded_columns):
                                 plt.figure(figsize=(10, 6))
 
                                 elements = order_data[order_type].unique()
-                                densities = order_data['Density'].unique()
+                                densities = order_data['density'].unique()
                                 plot_data = []
                                 plot_labels = []
 
                                 for element in elements:
                                     for density in densities:
                                         element_density_data = order_data[
-                                            (order_data[order_type] == element) & (order_data['Density'] == density)][
+                                            (order_data[order_type] == element) & (order_data['density'] == density)][
                                             column]
                                         plot_data.append(element_density_data)
                                         plot_labels.append(f"{element}_{density}")
@@ -77,7 +77,7 @@ def analyze_mtg_data(file_path, output_folder, excluded_columns):
 
                                 plt.xticks(positions, plot_labels, rotation=45)
                                 plt.ylabel(column)
-                                plt.title(f'Boxplot of {column} for all elements in {order_type} on {date}')
+                                plt.title('')
                                 plt.savefig(
                                     os.path.join(date_order_folder, f'boxplot_{order_type}_{column}.png'))
                                 plt.close()
@@ -85,11 +85,11 @@ def analyze_mtg_data(file_path, output_folder, excluded_columns):
                                 print(f"Error creating boxplot for {column} in {order_type} on {date}: {e}")
 
 
-file_path = "MTG.csv"
+file_path = "MTG_csv.csv"
 output_folder = "output_statistics_architecture"
-excluded_columns = ['phyllotaxis segments leaf (°)', 'Length segments leaf (mm)', 'Inclination segments leaf (°)',
-                    'Phyllotaxis 1st leaf segment (°)', 'Phyllotaxis 2nd leaf segment (°)', 'Phyllotaxis 3rd leaf segment (°)', 'Phyllotaxis 4th leaf segment (°)',
-                    'Length 1st segment (mm)', 'Length 2nd segment (mm)', 'Length 3rd segment (mm)', 'Length 4th segment (mm)',
-                    'Inclination 1st leaf segment (°)', 'Inclination 2nd leaf segment (°)', 'Inclination 3rd leaf segment (°)', 'Inclination 4th leaf segment (°)', ]
+excluded_columns = ['phyllotaxis_absolute_(°)', 'length_petiolule_(mm)', 'inclination_petiolule_(°)', 'total_leaf_area_(m2)', 
+                    'upper_transmittance', 'upper_reflectance',	'lower_transmittance', 'lower_reflectance',
+                    'quantum_efficiency', 'Amax', 'convexity'
+]
 
 analyze_mtg_data(file_path, output_folder, excluded_columns)
